927a3138a7f7d867c23e4a9f3fa22a66a75cb6a4
Kinnari Shah <kinnari1022@yahoo.com>
2020-09-12 17:14:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,7 +49,8 @@ class Student
   end
 
   def self.new_from_db(row)
-  student = Student.new(id, name, grade)
+    binding.pry
+  student = self.new(id, name, grade)
   end
   
   def self.find_by_name(name)
