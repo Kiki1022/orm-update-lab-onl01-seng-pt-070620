050e07bd0a8e615ef30159079c9336de19f3f2a7
Kinnari Shah <kinnari1022@yahoo.com>
2020-09-12 16:18:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,11 +47,19 @@ class Student
     student.save
     student
   end
-  
-  def update
-  
-  
+
+  def self.new_from_db
   
   end
   
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+      result = DB[:conn].execute(sql, name)[0]
+      Student.new(result[0], result[1], result[2])
+  end
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
+     DB[:conn].execute(sql, self.name, self.grade, self.name)
+  end    
 end
